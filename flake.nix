b3a1853b108293607ab4ce8b011fb2becbd4c397
Gabe Dunn <gabe@sent.at>
2024-08-02 20:24:03 -0600

    feat: add cache/substituters to flake, and one more time...

--- flake.nix
+++ flake.nix
@@ -44,6 +44,11 @@
     vtsls.inputs.nixpkgs.follows = "nixpkgs";
   };
 
+  nixConfig.extra-substituters =
+    "https://cache.nixos.org https://nix-community.cachix.org";
+  nixConfig.extra-trusted-public-keys =
+    "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY= nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=";
+
   # see :help nixCats.flake.outputs
   outputs = { self, nixpkgs, nixCats, ... }@inputs:
     let
